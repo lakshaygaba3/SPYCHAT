55ecddb673ac7601ef320f20787ce84a77cd602a
lakshay <lakshaygaba67@gmail.com>
2018-02-12 00:11:16 +0530

    status update complete

--- main.py
+++ main.py
@@ -1,15 +1,66 @@
-from spy_detail import spy_name, spy_rating, spy_salutation, spy_age
+from spy_detail import spy
 
 print "hello world"
-print  "let's get started"
+print "let's get started"
+STATUS_MESSAGES = ['JAMES BOND 007', 'JAGGA JASOOS', 'BYOKSHI', 'TUMSE NA HOGA SPY']
+friends = [{'name' : 'jatin', 'age' :25, 'rating' :5, 'is_online' :True},{'name': 'naveen', 'age' :28, 'rating':4, 'is_online':True}]
 
-def start_chat(spy_name,spy_age, spy_rating):
-    menu_choice = input("What do you want to do? \n 1. Add a status update \n 0. Close application")
+
+def add_status(C_S_M):
+    if C_S_M != None:
+        print "your current status is " + C_S_M
+    else:
+        print "you don't have any status"
+
+    user_choise = raw_input("do you want to select from old status? Y or N: ")
+
+    if user_choise.upper() == 'Y':
+        serial_no = 1
+        for old_status in STATUS_MESSAGES:
+            print str(serial_no) + ". " + old_status
+            serial_no = serial_no + 1
+
+        user_status_selection = input("Which one do u want to set this time? ")
+        new_status = STATUS_MESSAGES[user_status_selection - 1]
+
+    elif user_choise.upper() == 'N':
+        new_status = raw_input("write your status")
+        STATUS_MESSAGES.append(new_status)
+    else:
+        print "invalid entry"
+    return new_status
+
+def add_friend():
+    frnd = {
+        'name': '',
+        'age': 0,
+        'rating': 0.0,
+        'is_online': True
+    }
+    frnd['name'] = raw_input("write your frnd's name: ")
+    frnd_sal = raw_input("Mr. or Mrs. :")
+    frnd['name'] = frnd_sal + " " + frnd['name']
+    frnd['age'] = input("what's ur frnd age :")
+    frnd['rating'] = input("write your frnd rating :")
+
+    if len(frnd['name'])>2 and 50>=frnd['age']>=12 and frnd['rating'] >=spy['rating']:
+        friends.append(frnd)
+    else:
+        print "frnd with these value can't be added"
+    return len(friends)
+
+def start_chat(spy_name, spy_age, spy_rating):
+    current_status_message = None
     show_menu = True
     while show_menu:
+        menu_choice = input(
+            "What do you want to do? \n 1. Add a status update \n 2. Add a friend \n 0. Close application")
         if menu_choice == 1:
-            spy_status = raw_input("Write a status update")
-            print "your status is %s" % spy_status
+            updated_status_message = add_status(current_status_message)
+            print "your new status is updated to" + updated_status_message
+        elif menu_choice == 2:
+            no_of_frnds = add_friend()
+            print "i have " + str(no_of_frnds) + " friends"
         elif menu_choice == 0:
             show_menu = False
         else:
@@ -17,38 +68,43 @@ def start_chat(spy_name,spy_age, spy_rating):
             print "Invalid choice"
 
 
-existing = raw_input("Continue as " + spy_salutation + " " + spy_name + "(Y/N)?")
+existing = raw_input("Continue as " + spy['salutation'] + " " + spy['name'] + "(Y/N)?")
 if existing.upper() == "Y":
-      #Continue with the default user/details imported from the helper file.
-    print "Welcome %s %s age: %d Rating: %.1f Glad to have you back." % (spy_salutation,spy_name,spy_age,spy_rating)
-    start_chat(spy_name,spy_age,spy_rating)
+    # Continue with the default user/details imported from the helper file.
+    print "Welcome %s %s age: %d Rating: %.1f Glad to have you back." % (spy['salutation'], spy['name'], spy['age'], spy['rating'])
+    start_chat(spy['name'], spy['age'], spy['rating'])
 
 elif existing.upper() == "N":
-    spy_name = raw_input("Welcome to spy chat, you must tell me your spy name first: ")
+    spy = {
+        'name': '',
+        'salutation': 'Mr.',
+        'age': 0,
+        'rating': 0.0,
+        'is_online': True
+    }
+    spy['name'] = raw_input("Welcome to spy chat, you must tell me your spy name first: ")
 
-    if len(spy_name)>3:
-        print "welcome " + spy_name + " glad to meet you."
-        spy_salutation =raw_input("what should i cal you ? mr. or ms. ")
-        spy_name = spy_salutation + " " +spy_name
-        print spy_name
-        print "alright" + " " + " " + spy_name + " " + "i'd like to know a little more about u"
-        spy_age = 0
-        spy_rating = 0.0
-        spy_is_online = False
-        spy_age = input("what is your age? ")
-        if spy_age>12 or spy_age<50:
+    if len(spy['name']) > 3:
+        print "welcome " + spy['name'] + " glad to meet you."
+        spy_salutation = raw_input("what should i cal you ? mr. or ms. ")
+        spy['name'] = spy_salutation + " " + spy['name']
+        print spy['name']
+        print "alright" + " " + " " + spy['name'] + " " + "i'd like to know a little more about u"
+        spy['age'] = input("what is your age? ")
+        if spy['age'] > 12 or spy['age'] < 50:
             print "spy, your age is perfect"
-            spy_rating = input("what is your rating? ")
-            if spy_rating >=5.0:
+            spy['rating'] = input("what is your rating? ")
+            if spy['rating'] >= 5.0:
                 print "Great spy"
-            elif spy_rating<5.0 and spy_rating>=4.5:
+            elif spy['rating'] < 5.0 and spy['rating'] >= 4.5:
                 print "nice spy"
-            elif spy_rating<4.5 and spy_rating>=3.5:
+            elif spy['rating'] < 4.5 and spy['rating'] >= 3.5:
                 print "fine spy"
             else:
                 print "fuck off"
             spy_is_online = True
-            print "Authentication complete. Welcome  %s age: %d and rating of: %.1f Proud to have you onboard" % (spy_name, spy_age, spy_rating)
+            print "Authentication complete. Welcome  %s age: %d and rating of: %.1f Proud to have you onboard" % (
+                spy['name'], spy['age'], spy['rating'])
 
 
 
@@ -57,4 +113,4 @@ elif existing.upper() == "N":
     else:
         print "please enter a valid name of atleast 4 letter"
 else:
-     print "invalid responce"
+    print "invalid responce"
--- spy_detail.py
+++ spy_detail.py
@@ -1,4 +1,7 @@
-spy_name = 'Bond'
-spy_salutation = 'Dr.'
-spy_age = 23
-spy_rating = 4.7
+spy = {
+  'name': 'bond',
+  'salutation': 'Mr.',
+  'age': 24,
+  'rating': 4.7,
+  'is_online': True
+}
